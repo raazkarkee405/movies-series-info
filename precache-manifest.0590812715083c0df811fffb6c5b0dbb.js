56bedfe4488a109df752ede915826fdc00bdc575
Durgaraj <raazkarkee405@gmail.com>
2020-10-19 09:31:15 +0545

    Updates

---
+++ precache-manifest.0590812715083c0df811fffb6c5b0dbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f4b714ffb9a9a892cb080e6dfb151a",
+    "url": "/movies-series-info/index.html"
+  },
+  {
+    "revision": "4cf4f058059dcaa3403b",
+    "url": "/movies-series-info/static/css/main.d20d7e65.chunk.css"
+  },
+  {
+    "revision": "c48ae2d0585c8b5dedaf",
+    "url": "/movies-series-info/static/js/2.7b2ce002.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movies-series-info/static/js/2.7b2ce002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cf4f058059dcaa3403b",
+    "url": "/movies-series-info/static/js/main.f7ec2bd2.chunk.js"
+  },
+  {
+    "revision": "19e5972724a2c4c7b133",
+    "url": "/movies-series-info/static/js/runtime-main.3f11dc58.js"
+  }
+]);
