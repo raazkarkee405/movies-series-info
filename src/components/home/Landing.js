0d8375abf38d1703c039c5fc369d6decab7aed6d
Durgaraj <raazkarkee405@gmail.com>
2020-10-19 09:24:27 +0545

    initial commit

---
+++ src/components/home/Landing.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Spinner from '../layouts/Spinner'
+import MoviesContainer from './MoviesContainer'
+import SearchForm from './SearchForm'
+
+class Landing extends Component {
+    render() {
+        const {loading} = this.props
+        return (
+            <div className="container">
+                <SearchForm />
+                {loading ? <Spinner/> : <MoviesContainer />}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    loading: state.movies.loading
+})
+
+
+export default connect(mapStateToProps)(Landing)
